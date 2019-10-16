f65a71113fa6888f1315a6a088eef2d863df5bde
Martín Sande <sandecostamartin@gmail.com>
2019-10-16 18:39:29 +0200

    ADD parse abstract

--- crawler/spiders/arxiv.py
+++ crawler/spiders/arxiv.py
@@ -1,6 +1,7 @@
 import scrapy
 import io
 import PyPDF2
+from twisted.internet import defer
 
 
 class ArxivSpider(scrapy.Spider):
@@ -15,12 +16,7 @@ class ArxivSpider(scrapy.Spider):
 
     def __init__(self):
         self.docID = 0
-
-    def get_pdf_content_lines(response):
-        reader = PyPDF2.PdfFileReader(io.BytesIO(response.body))
-        for page in reader.pages:
-            for line in page.extractText().splitlines():
-                yield line
+        self.abstract = ''
 
     def parse(self, response):
         list_links = response.css('span.list-identifier')
@@ -30,14 +26,30 @@ class ArxivSpider(scrapy.Spider):
             list_authors = list_authors.css("a")
             list_titles = item.css('div.list-title')
             for x in range(len(list_titles)):
+                links = list_links[x].css('a::attr(href)').extract()
+                pdf_url = "https://arxiv.org"+links[1]
+                abstract_url = "https://arxiv.org"+links[0]
                 yield {'docID': self.docID,
-                       'title': list_titles[x].css('div.list-title::text').extract()[1][:-2],
-                       'authors': list_authors.css("a::text").extract(),
-                       'subjects': [item.css('span.primary-subject::text').get(), item.css('div.list-subjects::text').extract()[2][2:-2]],
-                       'abstract': list_links[x].css('a::attr(href)').extract()[0],
-                       'pdf': list_links[x].css('a::attr(href)').extract()[1]
-                       }
-                self.docID += 1
+                    'title': list_titles[x].css('div.list-title::text').extract()[1][:-2],
+                    'authors': list_authors.css("a::text").extract(),
+                    'subject': item.css('span.primary-subject::text').get(),
+                    'other-subjects': item.css('div.list-subjects::text').extract()[2][2:-2],
+                    'abstract': abstract_url,
+                    'pdf': pdf_url
+                }
+                self.docID+=1
+
+    def parse_abstract(self, response):
+        self.abstract = response.xpath('//*[@id="abs"]/blockquote/text()').extract()[0]
+
+
+    def parse_pdf(self, url):
+        request = scrapy.http.Response("https://arxiv.org"+url)
+        reader = PyPDF2.PdfFileReader(io.BytesIO(response.body))
+        for page in reader.pages:
+            for line in page.extractText().splitlines():
+                yield line
+
 
         #next_page = response.css('li.next a::attr(href)').get()
         # if next_page is not None:
