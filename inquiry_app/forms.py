870928293eb72e6eca0e73578be9f23d70077589
Martín Sande <sandecostamartin@gmail.com>
2019-11-15 20:04:01 +0100

    ADD search by date

--- inquiry_app/forms.py
+++ inquiry_app/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
+import datetime
 
 SUBJECT_CHOICES = [
     ('cs', 'Computer Science'),
@@ -13,17 +14,29 @@ class BasicSearchForm(forms.Form):
     keywords = forms.CharField(label="", max_length=100)
     subject = forms.CharField(label='', initial='', widget=forms.Select(choices=SUBJECT_CHOICES))
 
-class AdvancedSearchForm(forms.Form):
-    title = forms.CharField(label='Title', max_length=100, required=False)
-    authors = forms.CharField(label='Authors', max_length=100, required=False)
-    abstract = forms.CharField(label='Abstract', max_length=100, required=False)
-    content = forms.CharField(label='Content', max_length=100, required=False)
-    subject = forms.CharField(label='', initial='', widget=forms.Select(choices=SUBJECT_CHOICES), required=False)
-
     def clean(self):
         title = self.cleaned_data.get('title')
         authors = self.cleaned_data.get('authors')
         abstract = self.cleaned_data.get('abstract')
         content = self.cleaned_data.get('content')
         if not (title or authors or abstract or content):
+            raise ValidationError("You must specify at least one field")
+
+class AdvancedSearchForm(forms.Form):
+    title = forms.CharField(label='Title', max_length=100, required=False)
+    authors = forms.CharField(label='Authors', max_length=100, required=False)
+    abstract = forms.CharField(label='Abstract', max_length=100, required=False)
+    content = forms.CharField(label='Content', max_length=100, required=False)
+    subject = forms.CharField(label='', initial='', widget=forms.Select(choices=SUBJECT_CHOICES), required=False)
+    start_date = forms.DateField(initial=(datetime.datetime.now() - datetime.timedelta(days=1*365)), required=False)
+    end_date = forms.DateField(initial=datetime.date.today, required=False)
+
+    def clean(self):
+        title = self.cleaned_data.get('title')
+        authors = self.cleaned_data.get('authors')
+        abstract = self.cleaned_data.get('abstract')
+        content = self.cleaned_data.get('content')
+        start_date = self.cleaned_data.get('start_date')
+        end_date = self.cleaned_data.get('end_date')
+        if not (title or authors or abstract or content or (start_date and end_date)):
             raise ValidationError("You must specify at least one field")
