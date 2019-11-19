7bf9227a19c906278c96246c48e6bbf4bf9cfb15
Javier Garea <javier.garea@udc.es>
2019-11-19 16:22:10 +0100

    FIX multiword queries + ADD some search.html styles

--- inquiry_app/services.py
+++ inquiry_app/services.py
@@ -7,14 +7,34 @@ class InquiryService:
     def __init__(self):
         self.es = ElasticConnection().connection
 
+    def _extend_query(self, search, keywords):
+        search = search.suggest('suggestion', keywords, term={'field': 'abstract'})
+        request = search.execute()
+        suggestions = [elem['text'] for elem in request.suggest.suggestion[0]['options']]
+        suggestion_query = []
+
+        for suggestion in suggestions:
+            suggestion_query.append(Q('wildcard', abstract='*' + suggestion + '*'))
+
+        final_query = Q('bool',
+                should = suggestion_query,
+                minimum_should_match = 0)
+
+        search = search.query(final_query)
+
+        return search
+
     def search_by_keywords(self, keywords, subject):
-        search = Search(using=self.es, index="arxiv-index")
-        query_content = Q("wildcard", abstract="*" + keywords + "*")
+        search = Search(using=self.es, index='arxiv-index')
+        query_content = Q()
+        for keyword in keywords.split(' '):
+                query_content = query_content + \
+                    Q('wildcard', abstract='*' + keyword + '*')
         query_subject = Q()
         query_other = Q()
         if subject and subject != 'all':
-            query_subject = Q("wildcard", subject="*" + subject + ".*")
-            query_other = Q("wildcard", other_subjects="*" + subject + ".*")
+            query_subject = Q('wildcard', subject='*' + subject + '.*')
+            query_other = Q('wildcard', other_subjects='*' + subject + '.*')
         final_query = Q('bool',
                         must=[query_content],
                         should=[query_subject, query_other],
@@ -25,33 +45,16 @@ class InquiryService:
         search = search.highlight_options(order='score')
         search = search.highlight('abstract', fragment_size=400)
 
-        search = self.extend_query(search, keywords)
+        search = self._extend_query(search, keywords)
         request = search.execute()
-        
+
         for hit in request:
             response = hit.to_dict()
             response.update({'fragment': hit.meta.highlight.abstract})
             yield response
 
-    def extend_query(self, search, keywords):
-        search = search.suggest('suggestion', keywords, term={'field': 'abstract'})
-        request = search.execute()
-        suggestions = [elem["text"] for elem in request.suggest.suggestion[0]["options"]]
-        suggestion_query = []          
-
-        for suggestion in suggestions:
-            suggestion_query.append(Q("wildcard", abstract="*" + suggestion + "*"))
-
-        final_query = Q('bool',
-                should = suggestion_query,
-                minimum_should_match = 0)
-
-        search = search.query(final_query)
-
-        return search
-
     def search_by_fields(self, title, authors, abstract, content, subject, start_date, end_date):
-        search = Search(using=self.es, index="arxiv-index")
+        search = Search(using=self.es, index='arxiv-index')
         query_title = Q()
         query_authors = Q()
         query_subject = Q()
@@ -60,25 +63,29 @@ class InquiryService:
         query_content = Q()
 
         if title:
-            query_title = Q("wildcard", title="*" + title + "*")
+            for word in title.split(' '):
+                query_title = query_title + \
+                    Q('wildcard', title='*' + word + '*')
 
         if authors:
-            for author in authors.split(" "):
+            for author in authors.split(' '):
                 query_authors = query_authors + \
-                    Q("wildcard", authors="*" + author + "*")
+                    Q('wildcard', authors='*' + author + '*')
 
         if subject and subject != 'all':
-            query_subject = Q("wildcard", subject="*" + subject + ".*")
+            query_subject = Q('wildcard', subject='*' + subject + '.*')
             query_other = Q(
-                "wildcard", other_subjects="*" + subject + ".*")
+                'wildcard', other_subjects='*' + subject + '.*')
 
         if abstract:
-            query_abstract = Q("match", abstract=abstract)
-            search = search.highlight_options(order='score')
-            search = search.highlight('abstract', fragment_size=400)
+            for word in abstract.split(' '):
+                query_abstract = query_abstract + \
+                    Q('wildcard', abstract='*' + word + '*')
 
         if content:
-            query_content = Q("match", pdf=content)
+            for word in content.split(' '):
+                query_content = query_content + \
+                    Q('wildcard', content='*' + word + '*')
 
         # search = search.filter('range', submit_date={'gte': start_date , 'lte': end_date})
 
@@ -88,10 +95,18 @@ class InquiryService:
                         minimum_should_match=2)
 
         search = search.query(final_query)
+        search = search.source(
+            ['title', 'authors', 'subject', 'other_subjects', 'abstract', 'pdf_url'])
+
+        if abstract:
+            search = self._extend_query(search, abstract)
+            search = search.highlight_options(order='score')
+            search = search.highlight('abstract', fragment_size=400)
         request = search.execute()
+        
         for hit in request:
             response = hit.to_dict()
-            if abstract:
+            if 'highlight' in hit.meta:
                 response.update({'fragment': hit.meta.highlight.abstract})
             else:
                 response.update({'fragment': []})
