09080a3b7dbcde934352cef6ab4b797b02aea933
Javier Garea <javier.garea@udc.es>
2019-11-22 12:56:00 +0100

    ADD requirements.txt + RM databases from django settings

--- inquiry/settings.py
+++ inquiry/settings.py
@@ -78,10 +78,6 @@ WSGI_APPLICATION = 'inquiry.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
 }
 
 
